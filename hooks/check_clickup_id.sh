39425741aab557921e343b3caf20c3ea729c8c41
Vincenzo Romano <vincenzo.romano@newel.health>
2024-02-05 17:56:24 +0100

    handle merge commit message format

--- hooks/check_clickup_id.sh
+++ hooks/check_clickup_id.sh
@@ -3,16 +3,20 @@
 INPUT_FILE=$1
 COMMIT_MSG=$(head -n1 "$INPUT_FILE")
 
+REGEX_MERGE="^(Merge branch[a-zA-Z0-9!#$%ˆ&*()-=+._ ']+)$"
+
 REGEX='^\[(CU-[a-zA-Z0-9_ ]+)\]+([a-zA-Z0-9!#$%ˆ&*()-=+._ ]+)$'
 
-## checking commit-message format
-if ! [[ $COMMIT_MSG =~ $REGEX ]];
-then
-   echo -e "Your commit-message format is not valid:\n$COMMIT_MSG\n"
-   echo "Valid format examples:"
-   echo "[CU-xxx123] My commit message"
-   exit 1
-else
-   echo "Well done! Your commit-message is valid."
-   exit 0
+## checking commit-message merge format
+if ! [[ $COMMIT_MSG =~ $REGEX_MERGE ]]; then
+  ## checking commit-message format
+  if ! [[ $COMMIT_MSG =~ $REGEX ]]; then
+     echo -e "Your commit-message format is not valid:\n$COMMIT_MSG\n"
+     echo "Valid format examples:"
+     echo "[CU-xxx123] My commit message"
+     exit 1
+  else
+     echo "Well done! Your commit-message is valid."
+     exit 0
+  fi
 fi
