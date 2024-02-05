5a90d00fb1f971c11af283dce4a657b07be9448f
Vincenzo Romano <vincenzo.romano@newel.health>
2024-02-05 11:04:33 +0100

    removed python script + add sh script

--- the-hook/check_clickup_id.sh
+++ the-hook/check_clickup_id.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+COMMIT_MSG=$(git log -1 HEAD --pretty=format:%s)
+REGEX='^\[(CU+-[0-9]+)]([[:blank:]]+[[:alnum:]_'"'"'-]+)+$'
 
-if [[ "$MESSAGE" == *\[email\]* ]]; then
-  echo "do action here"
+## checking commit-message format
+if ! [[ $COMMIT_MSG =~ $REGEX ]];then
+    echo -e "Your commit-message format is not valid:\n$COMMIT_MSG\n"
+    echo "Valid format examples:"
+    echo "[CU-1234567] My commit message"
+    exit 1
 else
-  echo "do nothing"
+    echo "Well done! Your commit-message is valid."
+    exit 0
 fi
