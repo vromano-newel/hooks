b89ac208d9a21214bf583459abf9f955a4e352d1
Vincenzo Romano <vincenzo.romano@newel.health>
2024-02-05 12:30:55 +0100

    Update check_clickup_id.sh

--- hooks/check_clickup_id.sh
+++ hooks/check_clickup_id.sh
@@ -1,16 +1,15 @@
- #!/bin/bash
+#!/bin/bash
 
- COMMIT_MSG=$(git log -1 HEAD --pretty=format:%s)
- #REGEX='^\[(CU+-[:alnum:]+)]([[:blank:]]+[[:alnum:]_'"'"'-]+)+$'
- REGEX='^\[(CU-[a-zA-Z0-9_ ]+)\]+([a-zA-Z0-9_ ]+)$'
+COMMIT_MSG=$(git log -1 HEAD --pretty=format:%s)
+REGEX='^\[(CU-[a-zA-Z0-9_ ]+)\]+([a-zA-Z0-9_ ]+)$'
 
- ## checking commit-message format
- if ! [[ $COMMIT_MSG =~ $REGEX ]];then
-     echo -e "Your commit-message format is not valid:\n$COMMIT_MSG\n"
-     echo "Valid format examples:"
-     echo "[CU-xxx123] My commit message"
-     exit 1
- else
-     echo "Well done! Your commit-message is valid."
-     exit 0
- fi
+## checking commit-message format
+if ! [[ $COMMIT_MSG =~ $REGEX ]];then
+   echo -e "Your commit-message format is not valid:\n$COMMIT_MSG\n"
+   echo "Valid format examples:"
+   echo "[CU-xxx123] My commit message"
+   exit 1
+else
+   echo "Well done! Your commit-message is valid."
+   exit 0
+fi
