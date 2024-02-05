8d98a46b7c5e3bcfb43102202ea76a923db8eb8d
Vincenzo Romano <vincenzo.romano@newel.health>
2024-02-05 11:06:04 +0100

    [CU-test] prova commit

--- the-hook/check_clickup_id.sh
+++ the-hook/check_clickup_id.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 COMMIT_MSG=$(git log -1 HEAD --pretty=format:%s)
-REGEX='^\[(CU+-[0-9]+)]([[:blank:]]+[[:alnum:]_'"'"'-]+)+$'
+REGEX='^\[(CU+-[:alnum:]+)]([[:blank:]]+[[:alnum:]_'"'"'-]+)+$'
 
 ## checking commit-message format
 if ! [[ $COMMIT_MSG =~ $REGEX ]];then
     echo -e "Your commit-message format is not valid:\n$COMMIT_MSG\n"
     echo "Valid format examples:"
-    echo "[CU-1234567] My commit message"
+    echo "[CU-xxx123] My commit message"
     exit 1
 else
     echo "Well done! Your commit-message is valid."
